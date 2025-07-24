203ad68b1eecd19ddfb7a0c4c064bc5becb149d0
dhruvmojila <dhruvmojila098@gmail.com>
2025-07-24 02:33:39 -0400

    removed price key

--- src/pages/api/create-checkout.js
+++ src/pages/api/create-checkout.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
     mode: "subscription",
     payment_method_types: ["card"],
     customer: customer.id,
-    line_items: [{ price: "price_1RoHEpHSW88F8ZDjkK8hTOL1", quantity: 1 }], // use actual price ID
+    line_items: [{ price: process.env.STRIPE_PRODUCT_PRICE_KEY, quantity: 1 }], // use actual price ID
     success_url: `${req.headers.origin}/dashboard?checkout=success`,
     cancel_url: `${req.headers.origin}/dashboard?checkout=cancel`,
   });
